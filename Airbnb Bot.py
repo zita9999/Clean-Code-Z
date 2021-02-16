0e46224cdbc5428a9a29cdeeb1bfd86aa69dcc4f
Christopher Zita <53668760+zita9999@users.noreply.github.com>
2021-02-16 01:46:27 -0500

    Update Airbnb Bot.py

--- Airbnb Bot.py
+++ Airbnb Bot.py
@@ -4,6 +4,8 @@
  - I created a Youtube video that shows you exactly how to use this bot. I recommend you watch it by going to this link: https://youtu.be/V2u8asYjFcE
 
  - This bot is created in Python so to use it for yourself go to any Python application and copy and paste this entire code.
+ 
+ - If you want to learn how to create these type of bots for yourself you can take my course in web scraping to learn how. Here's the link: https://www.udemy.com/course/web-scraping-in-python-with-beautifulsoup-and-selenium/?referralCode=939EB64B8E029FCBBDEB
 
 '''
 
